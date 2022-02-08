119f5e5b292caff9c683b15321325454eb80884e
Christopher St. John <cks@panamplify.com>
2022-02-08 17:24:03 -0600

    first commit

---
+++ src/dickvandyke.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class DickVanDyke extends Component {
+    render() {
+        return (
+            <h1>Dick Van Dyke</h1>
+        )
+    }
+};
+
+DickVanDyke.label = "DickVanDyke";
+
+export default DickVanDyke;
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as DickVanDyke } from './dickvandkye.jsx';
